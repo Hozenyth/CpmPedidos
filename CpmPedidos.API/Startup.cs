caafea15dbf012f909f5ff0e3197fa4c0dbf706b
Hozenyth <hozenyth.andrade@gmail.com>
2021-12-19 11:17:46 -0300

    Configurado DbContext (Entity/ Npgsql (PostgreSQL).

--- CpmPedidos.API/Startup.cs
+++ CpmPedidos.API/Startup.cs
@@ -1,31 +1,36 @@
+using CpmPedidos.Repository.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Npgsql;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace CpmPedidos.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public DbConnection DbConnection => new NpgsqlConnection(Configuration.GetConnectionString("App"));
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(
+                    DbConnection,
+                    assembly => assembly.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+            });
+
             DependencyInjection.Register(services);
            
             services.AddControllers();
---
+++ CpmPedidos.Repository/Common/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.Repository.Common
+{
+    public class ApplicationDbContext :DbContext
+    {
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+        public ApplicationDbContext()
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+    }
+}
