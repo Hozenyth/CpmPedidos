63e5182ce07ee1c5dc7f4b6766c14981d4cc8428
Hozenyth <hozenyth.andrade@gmail.com>
2021-12-20 18:33:43 -0300

    feito Classe Endereco e enum Tipoendereço

---
+++ CpmPedidos.Domain/Entities/Endereco.cs
@@ -0,0 +1,20 @@
+﻿
+
+using CpmPedidos.Domain.Interfaces;
+
+namespace CpmPedidos.Domain.Entities
+{
+    public class Endereco : BaseDomain
+    {
+        public int EnderecoId { get; set; }      
+        public TipoEnderecoEnum Tipo{ get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }       
+       
+        public int CidadeId { get; set; }  
+        public virtual Cidade Cidade{ get; set; }  
+       
+    }
+}
