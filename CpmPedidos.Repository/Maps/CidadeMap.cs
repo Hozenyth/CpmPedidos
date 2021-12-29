d33cddcc762b7073973f4cb8e1f987e5386ef138
Hozenyth <hozenyth.andrade@gmail.com>
2021-12-29 17:50:17 -0300

    Impementando Map Entity.

--- CpmPedidos.Repository/Maps/CidadeMap.cs
+++ CpmPedidos.Repository/Maps/CidadeMap.cs
@@ -11,6 +11,10 @@ namespace CpmPedidos.Repository.Maps
         public override void Configure(EntityTypeBuilder<Cidade> builder)
         {
             base.Configure(builder);
+
+            builder.Property(x => x.Nome).HasColumnName("nome").HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Uf).HasColumnName("uf").HasMaxLength(2).IsRequired();
+            builder.Property(x => x.Ativo).HasColumnName("ativo").IsRequired();
         }
     }
 }
