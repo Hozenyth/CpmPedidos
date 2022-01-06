7d05f874ed2736a15b6b30e7cc2e33126043da83
Hozenyth <hozenyth.andrade@gmail.com>
2022-01-06 17:35:21 -0300

    Preparando pra Migrations

---
+++ CpmPedidos.Repository/Common/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace CpmPedidos.Repository.Common
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var enviromentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT");
+            var fileName = Directory.GetCurrentDirectory() + $"/../CpmPedidos.API/appsettings.{enviromentName}.json";
+            
+            var configuration = new ConfigurationBuilder().AddJsonFile(fileName).Build();
+            var connectionString = configuration.GetConnectionString("App");
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseNpgsql(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
